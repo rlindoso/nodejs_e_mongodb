31b050ae7d5a128f97f5d7a3d2c7792efba16009
Rafael Lindoso <rafael.lindoso@gmail.com>
2019-07-06 00:44:05 -0300

    aula 31 concluída

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
 
-module.exports = function() {
+var connMySQL = function(){
+    console.log('Conexão com o BD estabelecida');
     return mysql.createConnection({
         host: 'localhost',
         user: 'myUserName',
         password: 'myUserPassWd',
         database: 'portal_noticias'
     });
+}
+
+module.exports = function() {
+    console.log('O autoload carregou o módulo de conexão com o BD');
+    return connMySQL;
 }
