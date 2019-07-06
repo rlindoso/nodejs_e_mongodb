5e5f491e9fff942e248cdf79d9794bfc1d9bc1f7
Rafael Lindoso <rafael.lindoso@gmail.com>
2019-07-06 01:17:23 -0300

    aula 34

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -1,11 +1,11 @@
 module.exports = function(application){
-    application.get('/noticias', function(req, res){
+    application.get('/noticia', function(req, res){
         var connection = application.config.dbConnection();
 
         var noticiasModel = application.app.models.noticiasModel;
 
-        noticiasModel.getNoticias(connection, function(error, result){
-            res.render("noticias/noticias", {noticias: result});
+        noticiasModel.getNoticia(connection, function(error, result){
+            res.render("noticias/noticia", {noticia: result});
          });
     });
 
