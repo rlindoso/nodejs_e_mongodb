acee2b5d19b794be5f9a032b7f9bb6a7cd34f064
Rafael Lindoso <rafael.lindoso@gmail.com>
2019-07-06 00:20:12 -0300

    aula 30 do curso concluída

---
+++ app/routes/noticias.js
@@ -0,0 +1,17 @@
+var dbConnection = require('../../config/dbConnection');
+
+module.exports = function(app){
+
+    var connection = dbConnection();
+
+    connection.connect(function(err) {
+        if (err) throw err;
+        console.log("mySql Connected!");
+    });
+
+    app.get('/noticias', function(req, res){
+        connection.query('select * from noticias', function(error, result){
+            res.render("noticias/noticias", {noticias: result})
+         });
+    });
+}
